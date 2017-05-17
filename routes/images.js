c5d64e4fa0abd84023ab23d83cb5116869420a4a
def <defurni@gmail.com>
2017-05-17 10:26:06 -0500

    fixed a mistake in image route

--- routes/images.js
+++ routes/images.js
@@ -28,20 +28,18 @@ router.get('/:terms', function(req, res){
   let num = 'num=' + limit;
   let start = 'start=' + offset
   let url = `https://www.googleapis.com/customsearch/v1?${searchTerms}&${key}&${engine}&${num}&${start}`;
-  let date = new Date().toLocaleString()
-  search.saveSearchHistory(terms, date);
-  res.send('saved')
+
   //async processing response
-  // search.getContent(url)
-  //   .then((response) => {
-  //     data = JSON.parse(response)
-  //     data = search.formatImageData(data.items)
-  //     res.send(data)
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //     res.send(err.message)
-  //   })
+  search.getContent(url)
+    .then((response) => {
+      data = JSON.parse(response)
+      data = search.formatImageData(data.items)
+      res.send(data)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send(err.message)
+    })
 
 })
 
