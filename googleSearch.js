14589138dde218174a4409e27f87a3dd28d694da
def <defurni@gmail.com>
2017-05-17 10:21:57 -0500

    updated route for viewing image search history

--- googleSearch.js
+++ googleSearch.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
+var fs = require('fs');
 
 
 const getContent = function(url) {
@@ -31,8 +32,42 @@ const formatImageData = function (data) {
     return output;
 }
 
+const saveSearchHistory = function (terms, date) {
+  let newEntry = {
+    "term": terms,
+    "date": date
+  }
+  let history = []
+
+  fs.readFile(__dirname + '/imageSearchHistory', 'utf-8', function (err, data) {
+    if (err) console.log(err)
+    history = (data)? JSON.parse(data) : []
+    history.push(newEntry);
+    history = JSON.stringify(history)
+// need to set a limit... and does fs.writeFile work on heroku server?
+    fs.writeFile(__dirname + '/imageSearchHistory', history, function (err) {
+      if (err) throw err;
+      console.log('search history updated')
+    })
+  })
+}
+
+const getSearchHistory = function (limit) {
+  //TODO get onlly the limit's worth of history
+  return new Promise(function(resolve, reject){
+    let history;
+    fs.readFile(__dirname + '/imageSearchHistory', 'utf-8', function (err, data) {
+      if (err) console.log(err)
+      history = (data) ? JSON.parse(data) : "no history"
+      resolve(history)
+    });
+  })
+}
+
 
 module.exports = {
   getContent,
-  formatImageData
+  formatImageData,
+  saveSearchHistory,
+  getSearchHistory
 }
--- routes/images.js
+++ routes/images.js
@@ -5,11 +5,19 @@ var search = require('../googleSearch');
 let searchEngine = process.env.GOOGLE_ENGINE_ID;
 let apiKey = process.env.GOOGLE_API_KEY;
 
-router.get('/', function(req, res, next){
+router.get('/', function(req, res){
   res.render('images', {title:"image search API"})
 })
 
-router.get('/:terms', function(req, res, next){
+router.get('/history', function(req, res){
+  let limit = req.query.limit
+  search.getSearchHistory(limit)
+    .then(function(history){
+      res.json(history)
+    })
+})
+
+router.get('/:terms', function(req, res){
   //do some with googlesearch api...
   let terms = req.params.terms
   let offset = req.query.offset || 1
@@ -20,18 +28,20 @@ router.get('/:terms', function(req, res, next){
   let num = 'num=' + limit;
   let start = 'start=' + offset
   let url = `https://www.googleapis.com/customsearch/v1?${searchTerms}&${key}&${engine}&${num}&${start}`;
-
+  let date = new Date().toLocaleString()
+  search.saveSearchHistory(terms, date);
+  res.send('saved')
   //async processing response
-  search.getContent(url)
-    .then((response) => {
-      data = JSON.parse(response)
-      data = search.formatImageData(data.items)
-      res.send(data)
-    })
-    .catch((err) => {
-      console.error(err);
-      res.send(err.message)
-    })
+  // search.getContent(url)
+  //   .then((response) => {
+  //     data = JSON.parse(response)
+  //     data = search.formatImageData(data.items)
+  //     res.send(data)
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //     res.send(err.message)
+  //   })
 
 })
 
